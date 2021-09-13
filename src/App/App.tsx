8b6303f7432b263e59e5a9aeb6ba26bdd853aaed
ffayel <ffayel@gmail.com>
2021-09-13 13:14:58 +0100

    Add Button To app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button'
 
 function App() {
   return (
     <div className="App">
       Hello word ! Demat breizh!
+      <hr/>
+      <Button />
     </div>
   );
 }
---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css'
+
+const Button=()=> {
+    return (
+        <button className="Button">Salut!</button>
+    );
+}
+
+export default Button;
