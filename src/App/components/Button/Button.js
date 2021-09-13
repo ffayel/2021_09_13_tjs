2a12e1a499d6803376476b7c69ad87288fa84984
ffayel <ffayel@gmail.com>
2021-09-13 14:02:40 +0100

    add image to button

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -4,7 +4,7 @@ import './Button.css'
 const Button=(props)=> {
     console.log(props);
     return (
-        <button className="Button Button-primary">{props.text}</button>
+        <button className="Button Button-primary">{props.children}</button>
     );
 }
 
